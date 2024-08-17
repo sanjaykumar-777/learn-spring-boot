880dbd885fcf18e9f552deddaf152470b6d8bf47
sanjay-kumar777 <123931055+sanjay-kumar777@users.noreply.github.com>
2024-08-17 22:42:58 +0530

    first commit

---
+++ src/main/java/com/example/sanjay_kumar777/learn_spring_boot/LearnSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.sanjay_kumar777.learn_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sanjay_kumar777/learn_spring_boot/LearnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sanjay_kumar777.learn_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
