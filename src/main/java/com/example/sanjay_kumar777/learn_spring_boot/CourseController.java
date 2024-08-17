5c808875a7e50892f783accc4854419f565fc911
sanjay-kumar777 <123931055+sanjay-kumar777@users.noreply.github.com>
2024-08-17 23:10:22 +0530

    First rest api

---
+++ src/main/java/com/example/sanjay_kumar777/learn_spring_boot/CourseController.java
@@ -0,0 +1,19 @@
+package com.example.sanjay_kumar777.learn_spring_boot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @RequestMapping("/courses")
+    public List<Course> getAllCourses(){
+        return Arrays.asList(
+                new Course(1,"Learn Aws","Sanjay"),
+                new Course(2, "Learn Devops","Kumar")
+        );
+    }
+}
